c374fe6c5850440f16348d28f55c841de242439c
Charles-Lewis jaggi <charles-lewis.jaggi@he-arc.ch>
2019-01-17 08:30:25 +0100

    add comment to BinarySerialization class

--- OthelloJJ/BinarySerialization.cs
+++ OthelloJJ/BinarySerialization.cs
@@ -9,8 +9,19 @@ using System.Windows;
 
 namespace OthelloJJ
 {
+    /// <summary>
+    /// Class that manage serialization for the game
+    /// <source>https://stackoverflow.com/a/22417240</source>
+    /// </summary>
     class BinarySerialization
     {
+        /// <summary>
+        /// serialized a class into a file
+        /// </summary>
+        /// <typeparam name="T">type of class to serialized</typeparam>
+        /// <param name="filePath">path of the file</param>
+        /// <param name="objectToWrite">object to serialized and write in the file</param>
+        /// <param name="append">if we append on the file or rewrite it</param>
         public static void WriteToBinaryFile<T>(string filePath, T objectToWrite, bool append = false)
         {
 
@@ -22,7 +33,12 @@ namespace OthelloJJ
             
 
         }
-
+        /// <summary>
+        /// deserialize a binary file into class
+        /// </summary>
+        /// <typeparam name="T">Type of the class to deserialize</typeparam>
+        /// <param name="filePath">path of the file</param>
+        /// <returns></returns>
         public static T ReadFromBinaryFile<T>(string filePath)
         {
         
